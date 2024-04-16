abf921fe69321becd9f8e7448056a5da8b6d1404
Yuting Huang <yuting.huang@ucdconnect.ie>
2024-04-16 22:40:16 +0800

    1

--- badminton4.c
+++ badminton4.c
@@ -11,15 +11,15 @@ int main(int argc, char** argv) {
   MPI_Comm_size(MPI_COMM_WORLD, &world_size);
 
   int token;
-  int teammate_rank = (world_rank % 2 == 0) ? world_rank + 2 : world_rank - 2;
+  int teammate_rank = (world_rank + 1) % world_size;
 
   // Receive from the lower process and send to the higher process. Take care
   // of the special case when you are the first process to prevent deadlock.
   if (world_rank != 0) {
-    MPI_Recv(&token, 1, MPI_INT, world_rank - 1, 0, MPI_COMM_WORLD,
+    MPI_Recv(&token, 1, MPI_INT, (world_rank - 1 + world_size) % world_size, 0, MPI_COMM_WORLD,
              MPI_STATUS_IGNORE);
     printf("Process %d received token %d from process %d\n", world_rank, token,
-           world_rank - 1);
+           (world_rank - 1 + world_size) % world_size);
 
     // Notify teammate
     if(teammate_rank >= 0 && teammate_rank < world_size){
@@ -41,12 +41,6 @@ int main(int argc, char** argv) {
              MPI_COMM_WORLD);
     printf("Process %d sent token %d to process %d\n", world_rank, token,
            (world_rank + 1) % world_size);
-
-    // Notify teammate
-    if(teammate_rank >= 0 && teammate_rank < world_size){
-      MPI_Send(&token, 1, MPI_INT, teammate_rank, 0, MPI_COMM_WORLD);
-      printf("Process %d notified teammate %d\n", world_rank, teammate_rank);
-    }
   }
   // Now process 0 can receive from the last process. This makes sure that at
   // least one MPI_Send is initialized before all MPI_Recvs (again, to prevent
@@ -56,13 +50,11 @@ int main(int argc, char** argv) {
              MPI_STATUS_IGNORE);
     printf("Process %d received token %d from process %d\n", world_rank, token,
            world_size - 1);
-
-    // Notify teammate
+// Notify teammate
     if(teammate_rank >= 0 && teammate_rank < world_size){
       MPI_Send(&token, 1, MPI_INT, teammate_rank, 0, MPI_COMM_WORLD);
       printf("Process %d notified teammate %d\n", world_rank, teammate_rank);
     }
   }
-
   MPI_Finalize();
 }
